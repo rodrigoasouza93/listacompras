7808b369cf293f3554cf9a2b177e58dd2a61b962
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2019-02-28 18:22:21 -0300

    Adicionar a ação para o componente

--- src/createList/index.js
+++ src/createList/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import './list.css'
 import Form from './Form';
 import ListItem from './ListItem';
+import { Creators as ListActions } from '../store/actions/list';
 
-const CreateList = () => (
-  <div className="page-container">
-    
-    <Form />
-    
-    <div className="list-items-container">
-      <ListItem />
-    </div>
+class CreateList extends Component {
 
-  </div>
-);
+  addProduct = (produtc, list) => {
+    this.props.addProduct(produtc, list)
+  }
+  
+  render() {
+    return (
+      <div className="page-container">
 
-export default CreateList;
+        <Form addProduct={this.addProduct} />
+
+        <div className="list-items-container">
+          <ListItem />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  list: state.list
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(ListActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateList);
