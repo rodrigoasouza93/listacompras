506f775b0ec45eb2efc081b8c4bbe9519d195d00
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2019-02-28 18:09:15 -0300

    Criando action e reducer para lista e aplicando na store

---
+++ src/store/reducers/list.js
@@ -0,0 +1,18 @@
+import { Types } from '../actions/list';
+
+const initialState = {
+  list: null,
+  items: [],
+}
+export default function list(state=initialState, action) {
+  switch(action.type) {
+    case Types.ADD_PRODUCT:
+      return {
+        list: action.list,
+        items: [ ...state.items, action.product ]
+      }
+
+    default:
+      return state;
+  }
+}
