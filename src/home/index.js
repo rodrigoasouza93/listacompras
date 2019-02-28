f6f3d5dfc6bc9426cf2521b749fd4d6ec6031233
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2019-02-28 15:50:22 -0300

    Implementando a lista na home page

--- src/home/index.js
+++ src/home/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Divider from '@material-ui/core/Divider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons'
 
 import './home.css';
 
@@ -19,6 +23,34 @@ const Home = (props) => (
         </CardActionArea>
       </Card>
     </div>
+
+    <div className="list-container">
+      <Card className="card">
+        <CardActionArea className="card-action-area">
+          <CardContent className="card-content">
+            <div>
+              <p className="title">Mês</p>
+              <div className="list-card-body">
+                <div className="list-card-item">
+                  <FontAwesomeIcon icon={faShoppingBasket} size="sm" />
+                  <p>1 Item(s) Restante</p>
+                </div>
+                <div className="list-card-item">
+                  <FontAwesomeIcon icon={faCheck} size="sm" />
+                  <p>2 Item(s) Comprados</p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </CardActionArea>
+        <Divider />
+        <CardActions className="card-footer">
+          <p>01/01/2019</p>
+          <p>100.00</p>          
+        </CardActions>
+      </Card>
+    </div>
+
   </div>
 );
 
