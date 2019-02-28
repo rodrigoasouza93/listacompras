db46592989b483bc085a5b88a2eb32f94c9f7c05
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2019-02-28 17:03:00 -0300

    Criar e estilizar formulário

--- src/createList/index.js
+++ src/createList/index.js
@@ -1,7 +1,66 @@
 import React from 'react';
 
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { InputAdornment } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+
+import './list.css'
+
+const units = ['kg', 'lt', 'un'];
+
 const CreateList = () => (
-  <h1>Lista</h1>
+  <div className="page-container">
+    <form className="form-container">
+      <div className="form-row">
+        <TextField
+          label="Lista"
+          name="list"
+          value={''}
+          onChange={() => { }}
+          required
+        />
+        <Button variant="outlined" color="secondary">Adicionar</Button>
+      </div>
+      <div className="form-row">
+        <TextField
+          label="Produto"
+          name="product"
+          value={''}
+          onChange={() => { }}
+          required
+        />
+        <TextField
+          label="quantidade"
+          name="quantity"
+          value={''}
+          onChange={() => { }}
+          required
+        />
+        <TextField
+          select
+          label="Unidade"
+          name="unity"
+          value={''}
+          onChange={() => { }}
+          required
+        >
+          {units.map(option => (
+            <MenuItem key={option} calue={option}>{option}</MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          label="Preço"
+          name="price"
+          value={''}
+          onChange={() => { }}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">R$</InputAdornment>
+          }}
+        />
+      </div>
+    </form>
+  </div>
 );
 
 export default CreateList;
