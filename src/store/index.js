d625c20aa2e1d0dbb54fbaed6550f1f3de13bbf5
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2019-03-01 16:02:37 -0300

    Salvando a lista ao localStorage do navegador

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import { createStore } from 'redux'
 
 import reducers from './reducers';
+import { loadState, saveState } from './localStorage';
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const localStorageState = loadState();
+
+const store = createStore(
+  reducers,
+  localStorageState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => {
+  saveState({ list: store.getState().list })
+});
 
 export default store;
